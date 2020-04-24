550836a729a9e0d126fcf0bdf841fd42fb153f2b
Timothee Boingneres <timboing@icloud.com>
2020-04-24 18:23:04 +0200

    more seeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,25 +14,27 @@ u.email = "tim@tim.com"
 u.password = "timtim"
 u.save!
 
-p = Pet.new
-p.user = u
-p.pet_type = "Cat"
-p.name = Faker::Creature::Cat.name
-p.breed = Faker::Creature::Cat.breed
-p.gender = Faker::Creature::Dog.gender
-p.birth_date = Faker::Date.birthday(min_age: 1, max_age: 130)
-p.save!
+3.times do
+  p = Pet.new
+  p.user = u
+  p.pet_type = "Cat"
+  p.name = Faker::Creature::Cat.name
+  p.breed = Faker::Creature::Cat.breed
+  p.gender = Faker::Creature::Dog.gender
+  p.birth_date = Faker::Date.birthday(min_age: 1, max_age: 130)
+  p.save!
 
+  i = 0
+  1000.times do
+    i += 1
+    s = Stat.new
+    s.pet = p
+    s.category = "Heart"
+    s.value = rand * 30 + 110
+    s.time = Time.now - (60 * 60 * i)
+    s.save!
+  end
 
-i = 0
-1000.times do
-  i += 1
-  s = Stat.new
-  s.pet = p
-  s.category = "Heart"
-  s.value = rand * 30 + 110
-  s.time = Time.now - (60 * 60 * i)
-  s.save!
 end
 
 puts "Seed completed"
